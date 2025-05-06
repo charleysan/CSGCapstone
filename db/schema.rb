11523c63ad93cf02b1c052707c20d296934ebfb7
charleysan <hambley1997@gmail.com>
2025-05-06 19:23:58 +1000

    Made Users Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_30_111534) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_06_092322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_30_111534) do
     t.boolean "completed"
     t.string "category"
     t.datetime "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
